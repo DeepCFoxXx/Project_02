e5c92739f502d9e03afe5bebdebc6cfc37fd9c24
DeepCFoxXx <learncode23@gmail.com>
2018-01-07 17:38:18 +0000

    Triceratops discription done

--- src/main/java/herbivore_dinosaurs/Triceratops.java
+++ src/main/java/herbivore_dinosaurs/Triceratops.java
@@ -9,4 +9,7 @@ public class Triceratops extends HerbivoreDionsaurs {
         this.discription = discription;
     }
 
+    public String getDiscription() {
+        return "With its 3 horns, a parrot-like beak and a large frill that could reach nearly 1 metre (3 feet) across, the Triceratops skull is one of the largest and most striking of any land animal.";
+    }
 }
