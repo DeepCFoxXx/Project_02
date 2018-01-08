88fbcc06ec0854841e53bbf40027de65db0c7e90
DeepCFoxXx <learncode23@gmail.com>
2018-01-08 15:34:14 +0000

    70 Tests Passing

--- src/main/java/carnivore_dionsaurs/Utahraptor.java
+++ src/main/java/carnivore_dionsaurs/Utahraptor.java
@@ -1,15 +1,22 @@
-//package carnivore_dionsaurs;
-//
-//public class Utahraptor extends CarnivorousDinosaurs {
-//
-//    private String discription;
-//
-//    public Utahraptor(String name, int height, int length, int weight, String type, int healthValue) {
-//        super(name, height, length, weight, type, healthValue);
-//        this.discription = discription;
-//    }
-//
-//    public String getDiscription() {
-//        return "Largest known member of raptor family.";
-//    }
-//}
+package carnivore_dionsaurs;
+
+import dinosaurs_food.Edible;
+
+public class Utahraptor extends CarnivorousDinosaurs {
+
+    private String discription;
+
+    public Utahraptor(String name, int height, int length, int weight, String type, int healthValue) {
+        super(name, height, length, weight, type, healthValue);
+        this.discription = discription;
+    }
+
+    public String getDiscription() {
+        return "Largest known member of raptor family.";
+    }
+
+    public void feed(Edible edible){
+        //work out what happens when dinsosaur eats an edible
+        this.feed(edible);
+    }
+}
--- src/test/java/dinosaurs_test/carnivorous_dinosaurs_test/UtahraptorTest.java
+++ src/test/java/dinosaurs_test/carnivorous_dinosaurs_test/UtahraptorTest.java
@@ -1,59 +1,69 @@
-//package dinosaurs_test.carnivorous_dinosaurs_test;
-//
-//import carnivore_dionsaurs.Utahraptor;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class UtahraptorTest {
-//
-//    Utahraptor utahraptor;
-//
-//    @Before
-//    public void before() {
-//        utahraptor = new Utahraptor("Laura", 2, 4, 1000, "Carnivorous", 600);
-//    }
-//
-//    @Test
-//    public void utahraptorHasName() {
-//        assertEquals("Laura", utahraptor.getName());
-//    }
-//
-//    @Test
-//    public void utahraptorHasHeight() {
-//        assertEquals(2, utahraptor.getHeight());
-//    }
-//
-//    @Test
-//    public void utahraptorHasLength() {
-//        assertEquals(4, utahraptor.getLength());
-//    }
-//
-//    @Test
-//    public void utahraptorHasWeight() {
-//        assertEquals(1000, utahraptor.getWeight());
-//    }
-//
-//    @Test
-//    public void utahraptorHasType() {
-//        assertEquals("Carnivorous", utahraptor.getType());
-//    }
-//
-//    @Test
-//    public void utahraptorHasHealth() {
-//        assertEquals(600, utahraptor.getHealth());
-//    }
-//
-//    @Test
-//    public void utahraptorCanTakeDamage() {
-//        utahraptor.takeDamage(50);
-//        assertEquals(550, utahraptor.getHealth());
-//    }
-//
-//    @Test
-//    public void utahraptorHasDiscription() {
-//        assertEquals("Largest known member of raptor family.", utahraptor.getDiscription());
-//    }
-//
-//}
+package dinosaurs_test.carnivorous_dinosaurs_test;
+
+import carnivore_dionsaurs.Utahraptor;
+import dinosaurs_food.Edible;
+import dinosaurs_food.Pig;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UtahraptorTest {
+
+    Utahraptor utahraptor;
+    Edible edible;
+
+    @Before
+    public void before() {
+        utahraptor = new Utahraptor("Laura", 2, 4, 1000, "Carnivorous", 600);
+    }
+
+    @Test
+    public void utahraptorHasName() {
+        assertEquals("Laura", utahraptor.getName());
+    }
+
+    @Test
+    public void utahraptorHasHeight() {
+        assertEquals(2, utahraptor.getHeight());
+    }
+
+    @Test
+    public void utahraptorHasLength() {
+        assertEquals(4, utahraptor.getLength());
+    }
+
+    @Test
+    public void utahraptorHasWeight() {
+        assertEquals(1000, utahraptor.getWeight());
+    }
+
+    @Test
+    public void utahraptorHasType() {
+        assertEquals("Carnivorous", utahraptor.getType());
+    }
+
+    @Test
+    public void utahraptorHasHealth() {
+        assertEquals(600, utahraptor.getHealth());
+    }
+
+    @Test
+    public void utahraptorCanTakeDamage() {
+        utahraptor.takeDamage(50);
+        assertEquals(550, utahraptor.getHealth());
+    }
+
+    @Test
+    public void utahraptorCanFeed() {
+        edible = new Pig();
+        utahraptor.feed(150);
+        assertEquals(750, utahraptor.getHealth());
+    }
+
+    @Test
+    public void utahraptorHasDiscription() {
+        assertEquals("Largest known member of raptor family.", utahraptor.getDiscription());
+    }
+
+}
