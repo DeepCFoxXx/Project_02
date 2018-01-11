36b118c361f29158ec79389915e4fd7e73f39581
DeepCFoxXx <learncode23@gmail.com>
2018-01-11 14:06:09 +0000

    1st Attack Test Passed

--- src/test/java/dinosaur_attack_test/SwipeTest.java
+++ src/test/java/dinosaur_attack_test/SwipeTest.java
@@ -1,4 +1,28 @@
 package dinosaur_attack_test;
 
+import carnivore_dionsaurs.Allosaurus;
+import dinosaur_attack.Swipe;
+import dinosaurs.Dinosaur;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SwipeTest {
+
+    Swipe swipe;
+    Dinosaur dinosaur;
+
+    @Before
+    public void before() {
+      swipe = new Swipe();
+      dinosaur = new Allosaurus("Robert", 2, 5, 550, "Carnivorous", 2000);
+    }
+
+    @Test
+    public void canAttack() {
+        swipe.attack(dinosaur);
+        assertEquals(1800, dinosaur.getHealth());
+    }
+
 }
